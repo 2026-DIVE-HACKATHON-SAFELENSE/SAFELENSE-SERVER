c181e5b920901e4cd3d7a09f43814ba0ea420070
kimhoeyun <clnvrd@suwon.ac.kr>
2026-07-23 22:06:48 +0900

    feat: 카카오 로그인 유스케이스 추가

---
+++ src/main/kotlin/com/safelense/auth/application/KakaoLoginService.kt
@@ -0,0 +1,41 @@
+// 카카오 사용자 조회와 서비스 JWT 발급을 조합하는 로그인 유스케이스
+package com.safelense.auth.application
+
+import com.safelense.auth.kakao.KakaoApiClient
+import com.safelense.user.User
+import com.safelense.user.UserRepository
+import org.springframework.stereotype.Service
+
+data class KakaoLoginResult(
+    val accessToken: String,
+    val refreshToken: String,
+    val expiresIn: Long,
+    val isNewUser: Boolean,
+)
+
+@Service
+class KakaoLoginService(
+    private val kakaoApiClient: KakaoApiClient,
+    private val userRepository: UserRepository,
+    private val tokenIssuer: JwtTokenIssuer,
+) {
+    fun login(authorizationCode: String, redirectUri: String): KakaoLoginResult {
+        val kakaoUser = kakaoApiClient.getUser(authorizationCode, redirectUri)
+        val existingUser = userRepository.findByKakaoId(kakaoUser.id)
+        val user = existingUser ?: userRepository.save(
+            User(
+                kakaoId = kakaoUser.id,
+                nickname = kakaoUser.nickname,
+                profileImageUrl = kakaoUser.profileImageUrl,
+            ),
+        )
+        val tokens = tokenIssuer.issue(requireNotNull(user.id))
+
+        return KakaoLoginResult(
+            accessToken = tokens.accessToken,
+            refreshToken = tokens.refreshToken,
+            expiresIn = tokens.expiresIn,
+            isNewUser = existingUser == null,
+        )
+    }
+}
